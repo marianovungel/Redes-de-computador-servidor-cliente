38c0ed8f7b5282870759cf6eb6b7a9902628102e
marianovungel <vungem88@gmail.com>
2022-11-16 23:37:28 -0300

    new code

--- cliente.py
+++ cliente.py
@@ -1,15 +1,24 @@
+#fazer a importação
 from socket import *
+
+#noma do IP (Local)
 serverName = '127.0.0.1'
+
+#criar porta
 serverPort = 12075
 
+#Estabelecer a conexão com o método TCP (SOCK_STREAM)
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName, serverPort))
 
-sentence = input('digite a sua pesquisa: ').encode('utf-8')
+#digite a porta para se conectar
+sentence = input('Digite a porta para se conectar: ').encode('utf-8')
 
+#fazer o envio da mensagem
 clientSocket.send(sentence)
 modifedSentence = clientSocket.recv(2048)
 
+#resposta do servidor
 print(('do Servidor: '), modifedSentence.decode('utf-8'))
 
 clientSocket.close()
--- server.py
+++ server.py
@@ -1,16 +1,31 @@
+
+#importações
 from socket import *
 import os
+
+#criação da  porta do servidor
 serverPort = 12075
 
+#Estabelecer a conexão com o método TCP (SOCK_STREAM)
 serverSocket = socket(AF_INET, SOCK_STREAM)
+
+#passar a porta de conexão com o servidor
 serverSocket.bind(('', serverPort))
+
+#o servidor a espera de uma mensagem do cliente
 serverSocket.listen(1)
 
 print('servidor pronto para receber pedidos!')
 
 while 1:
+    #aceitar a conexão com o cliente
     connectionSocket, addr = serverSocket.accept()
+    
+    #validar a quantidade de bits a receber
     sentence = connectionSocket.recv(1024)
-    os.chdir('static')
+
+    #fazer o envio do arquivo html
     connectionSocket.send(os.chdir('static'))
+
+    #feixar a conexão do servidor
     connectionSocket.close()
