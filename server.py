88956d4d35efdce5a311c1eedf2218f0e7d96de6
marianovungel <vungem88@gmail.com>
2022-11-16 23:24:53 -0300

    codigo

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from socket import *
+import os
 serverPort = 12075
 
 serverSocket = socket(AF_INET, SOCK_STREAM)
@@ -10,7 +11,6 @@ print('servidor pronto para receber pedidos!')
 while 1:
     connectionSocket, addr = serverSocket.accept()
     sentence = connectionSocket.recv(1024)
-    capitalizeSentence = sentence.upper()
-    connectionSocket.send(capitalizeSentence)
-
+    os.chdir('static')
+    connectionSocket.send(os.chdir('static'))
     connectionSocket.close()
